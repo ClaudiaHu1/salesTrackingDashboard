ca159cf55f437915b7813e3688795d918d0138c9
claudia hu <bobliuyong2008@gmail.com>
2016-10-23 19:18:14 -0400

    update package.json and bower.json

--- public/controllers/analyticsController.js
+++ public/controllers/analyticsController.js
@@ -6,10 +6,14 @@
 trackingApp.controller('analyticsController', analyticsController);
 
 function analyticsController($scope, $http){
-    var googleData = {};
+    var googleDataSession = [], googleDataAvg = [], googleBanceRate = [];
     $http.get('testgoogledata.json').success(function(data) {
-        googleData = data;
-        console.log(JSON.stringify(googleData.sessions));
+        googleDataSession = data.sessions;
+        googleDataAvg = data.AvgSessionDuration;
+        googleBanceRate = data.BounceRate;
+
+        console.log(JSON.stringify(googleDataAvg));
+        console.log(JSON.stringify(googleDataSession));
 
         $(function () {
             $('#container').highcharts({
@@ -20,18 +24,7 @@ function analyticsController($scope, $http){
                     text: 'Sales Sessions'
                 },
                 xAxis: {
-                    categories: [
-                        "1/1/1904  12:26:55 AM",
-                        "1/1/1904  12:08:46 AM",
-                        "1/1/1904  12:01:03 AM",
-                        "1/1/1904  12:04:09 AM",
-                        "1/1/1904  12:01:39 AM",
-                        "1/1/1904  12:07:49 AM",
-                        "1/1/1904  12:06:21 AM",
-                        "1/1/1904  12:00:07 AM",
-                        "1/1/1904  12:00:45 AM",
-                        "1/1/1904  12:05:46 AM"
-                    ],
+                    categories: googleDataAvg,
                     title: {
                         text: 'Date'
                     }
@@ -61,67 +54,11 @@ function analyticsController($scope, $http){
 
                 series: [{
                     name: 'Sessions',
-                    data:
-                        [58,16,14,11,9,8,7,6,6,6]
+                    data: googleDataSession
 
                 }]
             });
         });
-
-    });
-
-
-
-
-
-
-        //var chart = new Highcharts.Chart({
-        //    chart: {
-        //        renderTo: '#barChart',
-        //        defaultSeriesType: 'column'
-        //        //events: {
-        //        //    load: googleData
-        //        //}
-        //    },
-        //    title: {
-        //        text: 'Google Analytics Data(July to August)'
-        //    },
-        //    subtitle: {
-        //        text: 'Source: Google Analytics'
-        //    },
-        //
-        //    xAxis: {
-        //        type: 'datetime',
-        //        crosshair: true
-        //    },
-        //
-        //    yAxis: {
-        //        min: 0,
-        //        title: {
-        //            text: 'Bounce Rate (%)'
-        //        }
-        //    },
-        //    tooltip: {
-        //        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        //        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-        //        '<td style="padding:0"><b>{point.y:.1f} %</b></td></tr>',
-        //        footerFormat: '</table>',
-        //        shared: true,
-        //        useHTML: true
-        //    },
-        //
-        //    plotOptions: {
-        //        column: {
-        //            pointPadding: 0.2,
-        //            borderWidth: 0
-        //        }
-        //    },
-        //
-        //    series: [{
-        //        //name: 'Random data',
-        //        data: googleData.session
-        //    }]
-        //});
 
     $(function () {
         $('#barChart').highcharts({
@@ -135,18 +72,7 @@ function analyticsController($scope, $http){
                 text: 'Source: google analytics'
             },
             xAxis: {
-                categories: [
-                    "1/1/1904  12:26:55 AM",
-                    "1/1/1904  12:08:46 AM",
-                    "1/1/1904  12:01:03 AM",
-                    "1/1/1904  12:04:09 AM",
-                    "1/1/1904  12:01:39 AM",
-                    "1/1/1904  12:07:49 AM",
-                    "1/1/1904  12:06:21 AM",
-                    "1/1/1904  12:00:07 AM",
-                    "1/1/1904  12:00:45 AM",
-                    "1/1/1904  12:05:46 AM"
-                ],
+                categories: googleDataAvg,
                 crosshair: true
             },
             yAxis: {
@@ -175,23 +101,10 @@ function analyticsController($scope, $http){
             },
             series: [{
                 name: 'Bounce Rate',
-                data: [
-                    13.79,
-                    18.75,
-                    50,
-                    72.73,
-                    66.67,
-                    0,
-                    42.86,
-                    83.33,
-                    88.33,
-                    33.33
-                ]
-
-
+                data: googleBanceRate
             }]
         });
     });
 
-
+    });
 }
